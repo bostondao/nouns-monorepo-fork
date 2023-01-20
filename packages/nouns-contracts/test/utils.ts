61e4ea4b05a3f0cde0a887a667c591a17d212383
colink14110476 <colink14110476@gmail.com>
2023-01-20 18:23:51 -0500

    rm unused code

--- packages/nouns-contracts/test/utils.ts
+++ packages/nouns-contracts/test/utils.ts
@@ -530,16 +530,3 @@ function dataToDescriptorInput(data: string[]): {
     itemCount,
   };
 }
-
-export const generateMerkleTree = (values: any[][]): StandardMerkleTree<any> => {
-    return StandardMerkleTree.of(values, ["address"])
-};
-
-export const generateMerkleProof = (tree: StandardMerkleTree<any>, address: string): string[] => {
-  for (const [i, v] of tree.entries()) {
-    if (v[0] == address) {
-      return tree.getProof(i)
-    }
-  }
-  return []
-};
