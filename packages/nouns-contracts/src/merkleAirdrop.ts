5d6d3be58b717c17d917257e067781e98f1a8bda
colink14110476 <colink14110476@gmail.com>
2023-03-23 16:15:22 -0400

    convert lowerCas

--- packages/nouns-contracts/src/merkleAirdrop.ts
+++ packages/nouns-contracts/src/merkleAirdrop.ts
@@ -6,7 +6,7 @@ export const generateMerkleTree = (values: any[][]): StandardMerkleTree<any> => 
 
 export const generateMerkleProof = (tree: StandardMerkleTree<any>, address: string): string[] => {
   for (const [i, v] of tree.entries()) {
-    if (v[0] == address) {
+    if (v[0].toLowerCase() == address.toLowerCase()) {
       return tree.getProof(i)
     }
   }
